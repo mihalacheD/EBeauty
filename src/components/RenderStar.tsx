c2e4b4c9666fc5b63615c6b6a34cc70b52db0305
mihalacheD <danutza_dima88@yahoo.com>
2025-03-11 15:33:06 +0200

    Update the RenderStar component

--- src/components/RenderStar.tsx
+++ src/components/RenderStar.tsx
@@ -1,23 +1,22 @@
 import { HStack } from "@chakra-ui/react";
 import { Star } from "lucide-react";
 
-
-
 const RenderStar = ({ rating }: { rating: number }) => {
+  // Rating-ul e întreg sau decimal, API returnează între 0 și 5
+  const fullStars = Math.round(rating);
 
   return (
     <HStack>
       {[...Array(5)].map((_, i) => (
         <Star
           key={i}
-          fill={i < rating / 2 ? "#FFD700" : "none"} // Umple stelele până la rating/2
+          fill={i < fullStars ? "#FFD700" : "none"} // Umple stelele până la rating
           stroke="#FFD700"
-          className="w-5 h-5" // Dimensiune standard pentru stele
+          className="w-5 h-5"
         />
       ))}
     </HStack>
   );
 };
 
-
-export default RenderStar
+export default RenderStar;
